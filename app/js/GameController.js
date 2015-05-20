3cc6d15153c566cfdcff924f18a445d9b9f24ea3
Xyzer <xyzer.mail@gmail.com>
2015-05-21 01:35:32 +0200

    Added tiles?

--- app/js/GameController.js
+++ app/js/GameController.js
@@ -1,10 +1,17 @@
 // GameController.js
-module.exports = function($scope, GameFactory){
+module.exports = function($scope, GameFactory, $http){
   $scope.games = GameFactory.games;
   $scope.addGame = function() {
     GameFactory.addGame();
   }
   $scope.joinGame = function(game) {
     GameFactory.joinGame(game);
+  }
+  $scope.getBoard = function(gameid) {
+    $http.get('https://mahjongmayhem.herokuapp.com/games/'+gameid+'/tiles')
+      .success(function(res) {
+        $scope.tiles = res;
+      });
+    // console.log($scope.tiles);
   }
 }
